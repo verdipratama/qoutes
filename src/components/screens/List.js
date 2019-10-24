ad15619586cc3748794de1a0e4c55f318b82adcf
Verdi Pratama <verdipratama@icloud.com>
2019-10-24 17:15:33 +0700

    Initial Commits

--- src/components/screens/List.js
+++ src/components/screens/List.js
@@ -28,30 +28,3 @@ const List = ({ list }) => {
 };
 
 export default List;
-
-/* <div className="container home">
-<div className="notes-wrapper">
-  <h2>Random Qoutes</h2>
-  <div className="note-item">
-
-    <div className="status">
-      <a href="#0" title="Mark as complete">
-        <Icon name="icon-check-cirle" size="24" />
-      </a>
-    </div>
- 
-    <div className="message">
-      <h3></h3>
-    </div>
-  
-    <div className="actions">
-      <a href="#0" title="Edit">
-        <Icon name="icon-edit" size="24" />
-      </a>
-      <a href="#0" title="Edit">
-        <Icon name="icon-edit" size="24" />
-      </a>
-    </div>
-  </div>
-</div>
-</div> */
--- src/components/screens/Warning.js
+++ src/components/screens/Warning.js
@@ -4,7 +4,7 @@ const Warning = () => {
   return (
     <div className="container empty__state">
       <div className="empty-state">
-        <h1>Sorry, no notes to show :(</h1>
+        <h1>Sorry, no qoutes to show :(</h1>
         <p>Waiting for update ...</p>
       </div>
     </div>
